7a756b5b0c580bd44f7fe3a3a570917d18027ea0
michaelramos02 <michaelramos1502@gmail.com>
2026-04-30 18:47:37 -0300

    Correção imc

--- aula08/atividade03.py
+++ aula08/atividade03.py
@@ -8,20 +8,20 @@ while resposta != 'N':
     altura = float(input('Digite a altura do aluno: '))
     peso = float(input('Digite o peso do aluno: '))
     resultado = calculoIMC(peso, altura)
-    if resultado > 40:
-         print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau III')
-    elif resultado >= 40:
-        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau II')
-    elif resultado > 35:
-        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau I')
-    elif resultado > 29.9:
-        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Acima do peso')
-    elif resultado > 24.9:
-        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Peso normal')
-    elif resultado > 18.4:
-        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Abaixo do peso')
-    else:
+    if resultado < 16.9 :
         print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Muito abaixo do peso')
+    elif resultado <= 18.4:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Abaixo do peso')
+    elif resultado <= 24.9:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Peso normal')
+    elif resultado <= 29.9:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Acima do peso')
+    elif resultado <= 34.9:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau I')
+    elif resultado <= 40:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau II')
+    else:
+        print(f'{aluno} está com {peso}kg e tem {altura:.2f}m o IMC é : {resultado:.2f} Obesidade grau III')
 
     resposta = input('Quer continuar? ').upper()
 
